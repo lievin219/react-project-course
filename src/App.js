b9f933edb8df58abc8ccae5f7eb74becf849baa3
lievin219 <gakizalievin219@gmail.com>
2024-05-03 00:44:09 +0200

    my react crash course deployment

--- src/App.js
+++ src/App.js
@@ -1,117 +1,117 @@
  import React, { useState, useEffect } from "react";
 
-import MovieCard from "./MovieCard";
+// import MovieCard from "./MovieCard";
 
-import "./App.css";
+// import "./App.css";
 
-const API_URL = "http://www.omdbapi.com?apikey=b6003d8a";
+// const API_URL = "http://www.omdbapi.com?apikey=b6003d8a";
 
-const App = () => {
-  const [searchTerm, setSearchTerm] = useState("");
-  const [movies, setMovies] = useState([]);
+// const App = () => {
+//   const [searchTerm, setSearchTerm] = useState("");
+//   const [movies, setMovies] = useState([]);
 
-  useEffect(() => {
-    searchMovies("Batman");
-  }, []);
+//   useEffect(() => {
+//     searchMovies("Batman");
+//   }, []);
 
-  const searchMovies = async (title) => {
-    const response = await fetch(`${API_URL}&s=${title}`);
-    const data = await response.json();
+//   const searchMovies = async (title) => {
+//     const response = await fetch(`${API_URL}&s=${title}`);
+//     const data = await response.json();
 
-    setMovies(data.Search);
-  };
+//     setMovies(data.Search);
+//   };
 
-  return (
-    <div className="app">
-      <h1>HLievin's MovieLand</h1>
+//   return (
+//     <div className="app">
+//       <h1>MovieLand</h1>
 
-      <div className="search">
-        <input
-          value={searchTerm}
-          onChange={(e) => setSearchTerm(e.target.value)}
-          placeholder="Search for movies"
-        />
-        <img src="https://raw.githubusercontent.com/gist/adrianhajdin/997a8cdf94234e889fa47be89a4759f1/raw/f13e5a9a0d1e299696aa4a0fe3a0026fa2a387f7/search.svg"
-          alt="search"
-          onClick={() => searchMovies(searchTerm)}
-        />
-      </div>
+//       <div className="search">
+//         <input
+//           value={searchTerm}
+//           onChange={(e) => setSearchTerm(e.target.value)}
+//           placeholder="Search for movies"
+//         />
+//         <img src="https://raw.githubusercontent.com/gist/adrianhajdin/997a8cdf94234e889fa47be89a4759f1/raw/f13e5a9a0d1e299696aa4a0fe3a0026fa2a387f7/search.svg"
+//           alt="search"
+//           onClick={() => searchMovies(searchTerm)}
+//         />
+//       </div>
 
-      {movies?.length > 0 ? (
-        <div className="container">
-          {movies.map((movie) => (
-            <MovieCard movie={movie} />
-          ))}
-        </div>
-      ) : (
-        <div className="empty">
-          <h2>No movies found </h2>
-        </div>
-      )}
-    </div>
-  );
-};
+//       {movies?.length > 0 ? (
+//         <div className="container">
+//           {movies.map((movie) => (
+//             <MovieCard movie={movie} />
+//           ))}
+//         </div>
+//       ) : (
+//         <div className="empty">
+//           <h2>No movies found</h2>
+//         </div>
+//       )}
+//     </div>
+//   );
+// };
 
-export default App;
-
-
+// export default App;
 
 
 
 
-// import './App.css' 
-// import MovieCard from './MovieCard'
+
+
+import './App.css' 
+import MovieCard from './MovieCard'
 
 
 
-// const API_URL = "http://www.omdbapi.com?apikey=e0d4628";
+const API_URL = "https://www.omdbapi.com?apikey=e0d4628";
 
-//  const App = ()=> {
-//     const [searchTerm, setSearchTerm] = useState("");
-//  const [movies, setMovies] = useState([]);
+ const App = ()=> {
+    const [searchTerm, setSearchTerm] = useState("");
+ const [movies, setMovies] = useState([]);
 
    
-//     const SearchMoies=async(title)=>{
-//         const response = await fetch(`${API_URL}&s=${title}`);
-//  const data=await response.json()
-//  console.log(data)
-//  setMovies(data.Search);
-//     }
+    const SearchMoies=async(title)=>{
+        const response = await fetch(`${API_URL}&s=${title}`);
+ const data=await response.json()
+ console.log(data)
+ setMovies(data.Search);
+    }
 
   
-// useEffect(()=>{
-//     SearchMoies('the flash')
-// },[])
-// return(
+useEffect(()=>{
+    SearchMoies('the flash')
+},[])
+return(
   
-//     <div className='app'>  
-//     <h1>Lievin's MovieLand</h1>
-//     <div className='search'>
-//         <input
-//         placeholder='Search A Movie'
-//         value={searchTerm}
-//         onChange={(e)=>setSearchTerm(e.target.value)
+    <div className='app'>  
+    <h1>Lievin's MovieLand</h1>
+    <div className='search'>
+        <input
+        placeholder='Search A Movie'
+        value={searchTerm}
+        onChange={(e)=>setSearchTerm(e.target.value)
                                   
-//         }/>
-//         <img src="https://raw.githubusercontent.com/gist/adrianhajdin/997a8cdf94234e889fa47be89a4759f1/raw/f13e5a9a0d1e299696aa4a0fe3a0026fa2a387f7/search.svg"
-//       alt="search"
-//       onClick={()=>SearchMoies(searchTerm)}
+        }/>
+        <img src="https://raw.githubusercontent.com/gist/adrianhajdin/997a8cdf94234e889fa47be89a4759f1/raw/f13e5a9a0d1e299696aa4a0fe3a0026fa2a387f7/search.svg"
+      alt="search"
+      onClick={()=>SearchMoies(searchTerm)}
      
-//     />
-//         </div>
+    />
+        </div>
      
-//          {movies?.length > 0 ? (
-//         <div className="container">
-//         {movies.map((movie) => (
-//           <MovieCard movie={movie} />
-//         ))}
-//       </div>
-//     ) : (
-//       <div className="empty">
-//         <h2>No movies found</h2>
-//       </div>
-//     )}
-//   </div>
-// );
-// };
-//    export default App 
+         {movies?.length > 0 ? (
+        <div className="container">
+        {movies.map((movie) => (
+          <MovieCard movie={movie} />
+        ))}
+      </div>
+    ) : (
+      <div className="empty">
+        <h2>No movies found</h2>
+      </div>
+    )}
+  </div>
+);
+};
+   export default App 
